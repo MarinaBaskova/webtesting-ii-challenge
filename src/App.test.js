07f1c38abf7f0eeddff42ca1e8d5f5f1aab41af8
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-05-14 15:39:05 -0700

    added test for app

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,56 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
+import App from './App';
+
+describe('<App />', () => {
+	it('renders without crashing', () => {
+		render(<App />);
+	});
+	// strikes
+	it('should successfully strike', () => {
+		const { getByText, getAllByText } = render(<App />);
+		const button = getAllByText(/Strike/i)[1];
+		fireEvent.click(button);
+		getAllByText(/1/)[1];
+	});
+	it('should successfully reset strike', () => {
+		const { getByText, getAllByText } = render(<App />);
+		const button = getAllByText(/Strike/i)[1];
+		fireEvent.click(button);
+		fireEvent.click(button);
+		fireEvent.click(button);
+		getAllByText(/0/)[1];
+	});
+	// balls
+	it('should successfully balls', () => {
+		const { getByText, getAllByText } = render(<App />);
+		const button = getAllByText(/Ball/i)[1];
+		fireEvent.click(button);
+		getAllByText(/1/)[1];
+	});
+	it('should successfully reset balls', () => {
+		const { getByText, getAllByText } = render(<App />);
+		const button = getAllByText(/Ball/i)[1];
+		fireEvent.click(button);
+		fireEvent.click(button);
+		fireEvent.click(button);
+		fireEvent.click(button);
+		getAllByText(/0/)[1];
+	});
+	// foul
+	it('should successfully fouls', () => {
+		const { getByText } = render(<App />);
+		const button = getByText(/Foul/i);
+		fireEvent.click(button);
+		fireEvent.click(button);
+		getByText(/2/);
+	});
+	// hit
+	it('should successfully hit and reset strikes and balls', () => {
+		const { getByText, getAllByText } = render(<App />);
+		const button = getByText(/Hit/i);
+		fireEvent.click(button);
+		getAllByText(/0/);
+	});
+});
